ca304a39b5758de2314f327260748acb2295d9ae
Gabriel Ilton <gabrielssantosjau@gmail.com>
2024-02-05 20:26:10 -0300

    feat: add component seletable ingredient

---
+++ src/components/SelectableIngredient.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import Tag from './Tag.vue';
+
+export default {
+	components: { Tag },
+	props: {
+		ingrediente: { type: String, required: true }
+	},
+	data() {
+		return { selecionado: false }
+	}
+}
+</script>
+
+<template>
+	<button class="ingrediente" @click="selecionado = !selecionado" :aria-pressed="selecionado">
+		<Tag :texto="ingrediente" :is_active="selecionado" />
+	</button>
+</template>
+
+<style scoped>
+.ingrediente {
+	cursor: pointer;
+}
+</style>
